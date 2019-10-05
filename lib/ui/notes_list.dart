092df052f0ea773e41c3fb7e75009610d40c0ea7
Momen Elzeiny <momenelzeiny172@gmail.com>
2019-10-05 04:00:03 +0200

    Create Notes List Screen

---
+++ lib/ui/notes_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NotesList extends StatefulWidget {
+  @override
+  _NotesListState createState() => _NotesListState();
+}
+
+class _NotesListState extends State<NotesList> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      
+    );
+  }
+}
+
+
